06697bf9487b1326de85cfd0ad444f81c73a8729
Daniel Burke <daniel.burke@forrent.com>
2016-03-26 17:04:59 -0400

    added constraints, connected outlets and actions

--- Switches/ViewController.swift
+++ Switches/ViewController.swift
@@ -57,7 +57,61 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     }
     
     func scrollViewDidScroll(scrollView: UIScrollView) {
-        print("Offset X: \(scrollView.contentOffset.x)")
+        
+        if prevButton != nil && nextButton != nil {
+            //Update the view based on the scrollView position
+            switch scrollView.contentOffset.x {
+            case 0:
+                prevButton.hidden = true;
+            case 0..<viewWidth:
+                prevButton.hidden = false;
+                logoImageView.alpha = 1 - (scrollView.contentOffset.x / viewWidth)
+                userLabel.alpha = scrollView.contentOffset.x / viewWidth
+                
+            case viewWidth..<viewWidth*2:
+                nextButton.hidden = false;
+                userLabel.alpha = 1 - (scrollView.contentOffset.x - viewWidth) / viewWidth
+                messageLabel.alpha = (scrollView.contentOffset.x - viewWidth) / viewWidth
+                
+            case viewWidth*2..<viewWidth*3:
+                nextButton.hidden = false;
+                messageLabel.alpha = 1 - (scrollView.contentOffset.x - viewWidth*2) / viewWidth
+                sendLabel.alpha = (scrollView.contentOffset.x - viewWidth*2) / viewWidth
+                
+            case viewWidth*3:
+                nextButton.hidden = true;
+                
+            default:
+                //() //Do nothing in this case
+                //
+                print("wasted cycles")
+            }
+            
+            //Update current index
+            currentIndex = Int(scrollView.contentOffset.x / viewWidth)
+        }
+    }
+    
+    //Don't use 0 as a tag ID
+    @IBAction func navigate(sender: AnyObject) {
+        if sender.isKindOfClass(UIButton) {
+            let button = sender as! UIButton
+            currentIndex = Int(scrollView.contentOffset.x / viewWidth)
+            var newOffsetX:CGFloat = 0.0
+            
+            switch (currentIndex, button.tag) {
+            case (1...4, 1):
+                newOffsetX = viewWidth * CGFloat(currentIndex - 1)
+            case (0...3, 2):
+                newOffsetX = viewWidth * CGFloat(currentIndex + 1)
+            default:
+                print("nothing")
+            }
+            
+            //Animate to scroll position
+            scrollView.setContentOffset(CGPointMake(newOffsetX, 0), animated: true)
+        }
+        
     }
 }
 
